f2a71d09e8d6985de853c87fcfc99e55c9c6d54f
vis2k <info@noobtuts.com>
2020-03-17 14:00:10 +0100

    CollisionInfo simplified

--- Source/Assets/CharacterController2k/Scripts/CollisionInfo.cs
+++ Source/Assets/CharacterController2k/Scripts/CollisionInfo.cs
@@ -6,58 +6,31 @@ namespace CharacterController2k
     public struct CollisionInfo
     {
         // The collider that was hit by the controller.
-        readonly Collider m_Collider;
+        public readonly Collider collider;
 
         // The controller that hit the collider.
-        readonly OpenCharacterController m_Controller;
+        public readonly OpenCharacterController controller;
 
         // The game object that was hit by the controller.
-        readonly GameObject m_GameObject;
+        public readonly GameObject gameObject;
 
         // The direction the character Controller was moving in when the collision occured.
-        readonly Vector3 m_MoveDirection;
+        public readonly Vector3 moveDirection;
 
         // How far the character has travelled until it hit the collider.
-        readonly float m_MoveLength;
+        public readonly float moveLength;
 
         // The normal of the surface we collided with in world space.
-        readonly Vector3 m_Normal;
+        public readonly Vector3 normal;
 
         // The impact point in world space.
-        readonly Vector3 m_Point;
+        public readonly Vector3 point;
 
         // The rigidbody that was hit by the controller.
-        readonly Rigidbody m_Rigidbody;
+        public readonly Rigidbody rigidbody;
 
         // The transform that was hit by the controller.
-        readonly Transform m_Transform;
-
-        // Gets the Collider associated with the collision
-        public Collider collider { get { return m_Collider; } }
-
-        // Gets the OpenCharacterController associated with the collision
-        public OpenCharacterController controller { get { return m_Controller; } }
-
-        // Gets the GameObject associated with the collision
-        public GameObject gameObject { get { return m_GameObject; } }
-
-        // Gets the move direction associated with the collision
-        public Vector3 moveDirection { get { return m_MoveDirection; } }
-
-        // Gets the length of the move associated with the collision
-        public float moveLength { get { return m_MoveLength; } }
-
-        // Gets the normal of the collision
-        public Vector3 normal { get { return m_Normal; } }
-
-        // Gets the point of the collision
-        public Vector3 point { get { return m_Point; } }
-
-        // Gets the Rigidbody associated with the collision
-        public Rigidbody rigidbody { get { return m_Rigidbody; } }
-
-        // Gets the Transform associated with the collision
-        public Transform transform { get { return m_Transform; } }
+        public readonly Transform transform;
 
         // Constructor
         // openCharacterController: The character controller that hit.
@@ -69,15 +42,15 @@ namespace CharacterController2k
                              Vector3 directionMoved,
                              float distanceMoved)
         {
-            m_Collider = hitInfo.collider;
-            m_Controller = openCharacterController;
-            m_GameObject = hitInfo.collider.gameObject;
-            m_MoveDirection = directionMoved;
-            m_MoveLength = distanceMoved;
-            m_Normal = hitInfo.normal;
-            m_Point = hitInfo.point;
-            m_Rigidbody = hitInfo.rigidbody;
-            m_Transform = hitInfo.transform;
+            collider = hitInfo.collider;
+            controller = openCharacterController;
+            gameObject = hitInfo.collider.gameObject;
+            moveDirection = directionMoved;
+            moveLength = distanceMoved;
+            normal = hitInfo.normal;
+            point = hitInfo.point;
+            rigidbody = hitInfo.rigidbody;
+            transform = hitInfo.transform;
         }
     }
 }
