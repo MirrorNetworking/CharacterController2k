c57213236fe93702c8438f307a3bfbb7fe5d42f5
vis2k <info@noobtuts.com>
2020-03-18 15:25:57 +0100

    IsMoveVectorAlmostZero tests

---
+++ Source/Assets/Tests/Editor/HelpersTests.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using NUnit.Framework;
+using UnityEngine;
+using UnityEngine.TestTools;
+
+namespace Controller2k.Tests
+{
+    public class HelpersTests
+    {
+        // A Test behaves as an ordinary method
+        [Test]
+        public void IsMoveVectorAlmostZero()
+        {
+            // it's almost zero if all components are smaller than the threshold
+            Assert.That(Helpers.IsMoveVectorAlmostZero(new Vector3(0,               0,               0),               Mathf.Epsilon), Is.True);
+            Assert.That(Helpers.IsMoveVectorAlmostZero(new Vector3(0,               0,               Mathf.Epsilon),   Mathf.Epsilon), Is.True);
+            Assert.That(Helpers.IsMoveVectorAlmostZero(new Vector3(0,               Mathf.Epsilon,   0),               Mathf.Epsilon), Is.True);
+            Assert.That(Helpers.IsMoveVectorAlmostZero(new Vector3(Mathf.Epsilon,   Mathf.Epsilon,   0),               Mathf.Epsilon), Is.True);
+            Assert.That(Helpers.IsMoveVectorAlmostZero(new Vector3(Mathf.Epsilon,   Mathf.Epsilon,   Mathf.Epsilon),   Mathf.Epsilon), Is.True);
+            Assert.That(Helpers.IsMoveVectorAlmostZero(new Vector3(Mathf.Epsilon,   Mathf.Epsilon,   Mathf.Epsilon),   Mathf.Epsilon), Is.True);
+            Assert.That(Helpers.IsMoveVectorAlmostZero(new Vector3(-Mathf.Epsilon,  Mathf.Epsilon,   Mathf.Epsilon),   Mathf.Epsilon), Is.True);
+            Assert.That(Helpers.IsMoveVectorAlmostZero(new Vector3(-Mathf.Epsilon,  Mathf.Epsilon,   -Mathf.Epsilon),  Mathf.Epsilon), Is.True);
+            Assert.That(Helpers.IsMoveVectorAlmostZero(new Vector3(Mathf.Epsilon*2, Mathf.Epsilon,   Mathf.Epsilon),   Mathf.Epsilon), Is.False);
+            Assert.That(Helpers.IsMoveVectorAlmostZero(new Vector3(Mathf.Epsilon,   Mathf.Epsilon*2, Mathf.Epsilon),   Mathf.Epsilon), Is.False);
+            Assert.That(Helpers.IsMoveVectorAlmostZero(new Vector3(Mathf.Epsilon,   Mathf.Epsilon,   Mathf.Epsilon*2), Mathf.Epsilon), Is.False);
+            Assert.That(Helpers.IsMoveVectorAlmostZero(new Vector3(1,               2,               3),               Mathf.Epsilon), Is.False);
+        }
+    }
+}
