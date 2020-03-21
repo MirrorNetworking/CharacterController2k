616379ffa939f502a1106dad07520702475b4c5f
vis2k <info@noobtuts.com>
2020-03-21 15:01:47 +0100

    Tests: IsSlideableAngle

---
+++ Source/Assets/Tests/Editor/CharacterController2kTests.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using NUnit.Framework;
+using UnityEngine;
+using UnityEngine.TestTools;
+
+namespace Controller2k.Tests
+{
+    public class CharacterController2kTests
+    {
+        [Test]
+        public void IsSlideableAngle()
+        {
+            // 90 degree is too steep to slide
+            Assert.That(CharacterController2k.IsSlideableAngle(90, 45), Is.False);
+
+            // 0 degree is too flat to slide
+            Assert.That(CharacterController2k.IsSlideableAngle(0, 45), Is.False);
+
+            // 30 degree is something but not enough for sliding
+            Assert.That(CharacterController2k.IsSlideableAngle(30, 45), Is.False);
+
+            // 50 degree is > 45, so we should slide
+            Assert.That(CharacterController2k.IsSlideableAngle(50, 45), Is.True);
+
+            // 85 degree is really steep, but still valid for sliding
+            Assert.That(CharacterController2k.IsSlideableAngle(85, 45), Is.True);
+        }
+    }
+}
