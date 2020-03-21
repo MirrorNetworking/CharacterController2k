a8f1084c34107ad418d4c0e449561e807fafc7c3
vis2k <info@noobtuts.com>
2020-03-21 18:31:54 +0100

    add comment

--- Source/Assets/CharacterController2k/ExampleScene/Scripts/PlayerMovement.cs
+++ Source/Assets/CharacterController2k/ExampleScene/Scripts/PlayerMovement.cs
@@ -143,6 +143,8 @@ public class PlayerMovement : MonoBehaviour
         // only while grounded, so jump key while jumping doesn't start a new
         // jump immediately after landing
         // => and not while sliding, otherwise we could climb slides by jumping
+        // => not even while SlidingState.Starting, so we aren't able to avoid
+        //    sliding by bunny hopping.
         // => grounded check uses min fall tolerance so we can actually still
         //    jump when walking down steps.
         return isGroundedWithinTolerance &&
